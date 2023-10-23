7a570948e2bad4619691098766760f8a8eb360da
Replit user <>
2023-10-22 22:30:00 -0400

    update menampilkan halaman index

--- myApp.js
+++ myApp.js
@@ -1,16 +1,13 @@
 let express = require('express');
 let app = express();
 
+// Baris ini mendefinisikan variabel absolutePath yang berisi path absolut ke file index.html yang berada di dalam folder views. __dirname adalah variabel global dalam Node.js yang berisi direktori dari file script yang sedang dieksekusi.
+absolutePath = __dirname + '/views/index.html';
 
-
-
-console.log("Hello World");
-
+// Baris ini mendefinisikan rute GET ke root URL aplikasi Anda (‘/’). Ketika seseorang mengunjungi root URL, fungsi callback yang didefinisikan akan dipanggil.
 app.get('/', function(req, res) {
-  res.send('Hello Express');
+  res.sendFile(absolutePath);
 });
-
-
 
 
 
