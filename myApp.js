bcdaa7202d41fba2bcf2015f63eea52021fdd7a5
GrimsAlphaDev <94513182+GrimsAlphaDev@users.noreply.github.com>
2023-10-23 02:00:11 +0000

    first method using get method

--- myApp.js
+++ myApp.js
@@ -4,6 +4,11 @@ let app = express();
 
 
 
+console.log("Hello World");
+
+app.get('/', function(req, res) {
+  res.send('Hello Express');
+});
 
 
 
@@ -35,5 +40,4 @@ let app = express();
 
 
 
-
- module.exports = app;
+module.exports = app;
