75b66d4bb49a95ec18ef57da2cea97605e0d2946
Replit user <>
2023-10-22 22:51:35 -0400

    update use asset

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 let express = require('express');
 let app = express();
 
+// baris kode ini pada dasarnya menginstruksikan server Express untuk melayani file statis dari direktori /public Anda setiap kali ada permintaan ke jalur /public
+app.use("/public", express.static(__dirname + "/public"));
+
 // Baris ini mendefinisikan variabel absolutePath yang berisi path absolut ke file index.html yang berada di dalam folder views. __dirname adalah variabel global dalam Node.js yang berisi direktori dari file script yang sedang dieksekusi.
 absolutePath = __dirname + '/views/index.html';
 
